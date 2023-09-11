e1eedbecd4a62a96417d2dd2ff01bb43bb8ef4d4
Smurillo0 <smurillo.0103@gmail.com>
2023-09-10 19:51:48 -0500

    Added Prototype1

---
+++ Prototype1/Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Sarahi Murillo
+ CGE 401 
+ Assignment 2
+ September 10, 2023
+
+ */
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
