6470fb0d69d6872f0b49205a16fb787def01b1ea
Smurillo0 <smurillo.0103@gmail.com>
2023-10-01 01:16:12 -0500

    Prototype3

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode forceMode;
+    public float gravityModifier;
+
+
+
+    public bool isOnGround = true;
+    public bool gameOver = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set reference to rigidbody component
+        rb = GetComponent<Rigidbody>();
+
+        forceMode = ForceMode.Impulse;
+
+        //modify gravity
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
